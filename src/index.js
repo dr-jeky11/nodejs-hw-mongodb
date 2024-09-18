4613eb213d41c7182119ccf0a61e87b8cc39a16d
dr-jeky11 <tyrsyna@gmail.com>
2024-09-18 15:13:33 +0200

    updated with contacts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { initMongoDB } from './db/initMongoDB.js';
-import startServer from './server.js';
+import initMongoConnection from './db/initMongoConnection.js';
+import setupServer from './server.js';
 
-const bootstrap = async () => {
-  await initMongoDB();
-  startServer();
-};
-
-bootstrap();
+await initMongoConnection();
+setupServer();
