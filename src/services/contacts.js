7bc4bae1d5537a7729e264862e07b3debf1c8d60
dr-jeky11 <tyrsyna@gmail.com>
2024-09-25 13:11:30 +0200

    services and controller

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,21 @@
 import { ContactsCollection } from '../db/models/contact.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
-};
+export const getAllContacts = () => ContactsCollection.find();
 
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
+export const getContactById = (contactId) =>
+  ContactsCollection.findById(contactId);
+
+export const createContact = (payload) => ContactsCollection.create(payload);
+
+export const updateContact = (contactId, payload, options = {}) => {
+  const contact = ContactsCollection.findByIdAndUpdate(contactId, payload, {
+    new: true,
+    runValidators: true,
+    ...options,
+  });
+  if (!contact) return null;
   return contact;
 };
+
+export const deleteContactById = (contactId) =>
+  ContactsCollection.findByIdAndDelete(contactId);
