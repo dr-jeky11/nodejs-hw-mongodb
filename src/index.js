8e2b85bdd58d0797edd7d0224df3fb8d7758cefb
dr-jeky11 <tyrsyna@gmail.com>
2024-10-23 14:45:09 +0200

    multer, router for photos, createDir

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import initMongoConnection from './db/initMongoConnection.js';
-import setupServer from './server.js';
+import { initMongoDB } from './db/initMongoDB.js';
+import { startServer } from './server.js';
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
-await initMongoConnection();
-setupServer();
+const bootstrap = async () => {
+  await initMongoDB();
+  await createDirIfNotExists(TEMP_UPLOAD_DIR);
+  await createDirIfNotExists(UPLOAD_DIR);
+  startServer();
+};
+
+void bootstrap();
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFound } from './middlewares/notFound.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
+import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -22,14 +22,6 @@ const setupServer = () => {
 
   app.use(cookieParser());
 
-  // app.use(
-  //   pino({
-  //     transport: {
-  //       target: 'pino-pretty',
-  //     },
-  //   }),
-  // );
-
   app.get('/', (req, res) => {
     res.status(200).json({
       status: 200,
@@ -42,6 +34,8 @@ const setupServer = () => {
   app.use(errorHandler);
 
   app.use(notFound);
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
